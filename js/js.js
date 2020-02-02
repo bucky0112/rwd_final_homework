b3f62b7b93de899cb30803278dd0f3e3bc0d60ae
bucky0112 <bucky0112@gmail.com>
2020-02-03 00:40:40 +0800

    暫時完成cart.html,剩下js部分還沒完成

--- js/js.js
+++ js/js.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-    $('.showmenu').on('click',  function(e){
+
+  // 漢堡選單
+  $('.showmenu').on('click',  function(e){
        e.preventDefault();
        $('body').toggleClass('menu-show');
    });
+
+
+  //  點擊喜歡的商品，愛心變化
+  $('.love_this').click(function(e){
+    e.preventDefault();
+    if($('#hollow_heart').attr('class') == 'far fa-heart'){
+      $('#hollow_heart').attr('class', 'fas fa-heart');
+    } else {
+      $('#hollow_heart').attr('class', 'far fa-heart')
+    }
+  })
  });
