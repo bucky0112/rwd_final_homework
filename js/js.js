4300c18d898c7b2781e21e0af8909db8bcc1fbbc
bucky0112 <bucky0112@gmail.com>
2020-02-04 00:05:38 +0800

    完成點擊愛心JS和購物車數字提示

--- js/js.js
+++ js/js.js
@@ -1,19 +1,15 @@
-$(document).ready(function() {
 
-  // 漢堡選單
+// 漢堡選單
+$(function() {
   $('.showmenu').on('click',  function(e){
        e.preventDefault();
        $('body').toggleClass('menu-show');
    });
-
+ });
 
   //  點擊喜歡的商品，愛心變化
-  $('.love_this').click(function(e){
-    e.preventDefault();
-    if($('#hollow_heart').attr('class') == 'far fa-heart'){
-      $('#hollow_heart').attr('class', 'fas fa-heart');
-    } else {
-      $('#hollow_heart').attr('class', 'far fa-heart')
-    }
-  })
- });
+ $(function(){
+  $('.love_this').click(function(){
+    $(this).find('i').toggleClass('far fa-heart fas fa-heart')
+  });
+});
