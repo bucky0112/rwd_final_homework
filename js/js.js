853cc933ad81f481f25deb377b6af01f8918dbe7
bucky0112 <bucky0112@gmail.com>
2020-02-04 22:32:48 +0800

    新增點擊加入購物車按鈕，在購物車icon顯示數字累加功能

--- js/js.js
+++ js/js.js
@@ -1,15 +1,27 @@
 
 // 漢堡選單
-$(function() {
-  $('.showmenu').on('click',  function(e){
-       e.preventDefault();
-       $('body').toggleClass('menu-show');
-   });
- });
+$(function () {
+  $('.showmenu').on('click',  function (e) {
+    e.preventDefault();
+    $('body').toggleClass('menu-show');
+  });
+});
 
   //  點擊喜歡的商品，愛心變化
- $(function(){
-  $('.love_this').click(function(){
-    $(this).find('i').toggleClass('far fa-heart fas fa-heart')
+$(function () {
+  $('.love_this').on('click', function (e) {
+    e.preventDefault();
+    $(this).find('i').toggleClass('far fa-heart fas fa-heart');
+  });
+});
+
+
+// 點擊加入購物車，顯示數字累加
+$(function () {
+  var count = 0;
+  $('.add_to_cart').on('click', function (e) {
+    e.preventDefault();
+    count = count ++;
+    $('.add_number').html(count);
   });
 });
